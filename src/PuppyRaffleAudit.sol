5d2119a838f25da5ebaaf0b2757f315626b0b17b
CheshireSec <brokenpanda@protonmail.com>
2024-06-17 18:40:58 +0200

    add final audited contract

--- src/PuppyRaffleAudit.sol
+++ src/PuppyRaffleAudit.sol
@@ -85,10 +85,15 @@ contract PuppyRaffle is ERC721, Ownable {
     /// @notice they have to pay the entrance fee * the number of players
     /// @notice duplicate entrants are not allowed
     /// @param newPlayers the list of players to enter the raffle
-    // v: if msg.value is 0, and newPlayers.length is also 0, the require statement will pass (which it shouldn't). Then the first for loop is skipped
-    // i: executing the function with msg.value=0 and newPlayers=[], seems to result in a infinite loop, running out of gas after some time. Note this down and investigate further.
-    // v: the newPlayers array gets added to the players array before it is checked for duplicate entry
-    // i: if the require() is false, the whole transaction seems to be reverted, so the players array is not modified as initially thought.
+    //
+    // v: [M-1] if the provided msg.value is 0, and newPlayers=[], the require statement will pass (which it shouldn't).
+    //    In this case, the first for loop is skipped and the second for loop will check the players array for duplicates (which shouldn't be present). But no harm is being caused.
+    //    If the `players` array had no entries, the transaction will run out of gas due to an Underflow exception at: `for (uint256 i = 0; i < players.length - 1; i++) { ... }`. This could maybe function as a DOS attack?
+    //
+    // i: the newPlayers array gets added to the players array before it is checked for duplicate entry
+    // i: if the require() statement is false, the whole transaction seems to be reverted, so the players array is not modified as initially thought.
+    // v: [H-3] A player can enter multiple addresses, as he can deploy contracts that he controls, increasing his chance to win the raffle
+    // v: [M-2] The duplicate check does not account for zero addresses. If more than one zero address is in he `players` array, no new players can enter the raffle anymore
     function enterRaffle(address[] memory newPlayers) public payable {
         require(msg.value == entranceFee * newPlayers.length, "PuppyRaffle: Must send enough to enter raffle");
         for (uint256 i = 0; i < newPlayers.length; i++) {
@@ -97,9 +102,9 @@ contract PuppyRaffle is ERC721, Ownable {
 
         // Check for duplicates
         // i: seems to really check for duplicates
-        // i: if players.length is 0, the loop will throw an Underflow exception as it equals to i < -1
-        // i: if players.length is 1, the loop will be skipped as i < 0 is already false in the first run
-        // i: but you need to verify that before marking it as a finding
+        // v: [M-1] if players.length is 0, the loop will throw an Underflow exception as it equals to i < -1
+        // i: But if the `newPlayers` array is not empty, doing `players.length-1` will be no issue, as addresses are added to the array before checking for duplicates. But the user will be paying to add nothing to the `players` array.
+        // i: if players.length is 1, the loop will be skipped as i < 0 is already false in the first run. But that's okay as there is only 1 entry in the array and no duplicates can be present
         for (uint256 i = 0; i < players.length - 1; i++) {
             for (uint256 j = i + 1; j < players.length; j++) {
                 require(players[i] != players[j], "PuppyRaffle: Duplicate player");
@@ -110,10 +115,11 @@ contract PuppyRaffle is ERC721, Ownable {
 
     /// @param playerIndex the index of the player to refund. You can find it externally by calling `getActivePlayerIndex`
     /// @dev This function will allow there to be blank spots in the array
-    // i: If the line `payable(msg.sender).sendValue(entranceFee)` fails at the wrong time, it can send the money but not reset the players address, resulting in infinite refunds
-    // i: there is no check to see if the raffle was won by the player. If the player won the raffle, he could refund his entranceFee and still get the prize, resulting in a loss for the protocol
-    // i: there is no check to see if the provided playerIndex is present in the `players` array. Providing an invalid playerIndex throws an `invalid opcode` exception
-    // i: The 
+    // v: [H-1] If the line `payable(msg.sender).sendValue(entranceFee)` is re-entered, an attacker can refund fees multiple times
+    // v: [H-2] There is no check to see if the raffle was won by the player. If the player won the raffle, he could refund his entranceFee and still get the prize, resulting in a loss for the protocol
+    // v: [H-2] Setting the players index to the zero address may be bad. If this player index gets selected as a winner, the reward will be sent to the zero address and be forever lost
+    // v: [M-2] Once 2 players have refunded, there are 2 zero address entries in the `players` array. This means calling `enterRaffle` will be impossible as the require statement `require(players[i] != players[j], "PuppyRaffle: Duplicate player");` will always fail and revert the transaction, stopping new players from entering
+    // i: There is no check to see if the provided playerIndex is present in the `players` array. Providing an invalid playerIndex throws an `invalid opcode` exception (index out of range)
     function refund(uint256 playerIndex) public {
         address playerAddress = players[playerIndex];
         require(playerAddress == msg.sender, "PuppyRaffle: Only the player can refund");
@@ -128,8 +134,8 @@ contract PuppyRaffle is ERC721, Ownable {
     /// @notice a way to get the index in the array
     /// @param player the address of a player in the raffle
     /// @return the index of the player in the array, if they are not active, it returns 0
-    // i: when the playerIndex of the player's address is 0, it returns also 0 - indicating that the player is not active when he is.
-    // i: before calling this function, add a check if the player is active
+    // v: [I-2] When the playerIndex of the player's address is 0, it returns also 0 - indicating that the player is not active when he actually is.
+    //    before calling this function, add a check if the player is active like `require(_isActivePlayer)`
     function getActivePlayerIndex(address player) external view returns (uint256) {
         for (uint256 i = 0; i < players.length; i++) {
             if (players[i] == player) {
@@ -145,14 +151,18 @@ contract PuppyRaffle is ERC721, Ownable {
     /// @dev we use a hash of on-chain data to generate the random numbers
     /// @dev we reset the active players array after the winner is selected
     /// @dev we send 80% of the funds to the winner, the other 20% goes to the feeAddress
-    // i: there could be a replay attack, where the attacker calls selectWinner when the statement
-    // i: `uint256(keccak256(abi.encodePacked(msg.sender, block.timestamp, block.difficulty))) % players.length` results in his playerIndex 
+    // v: [H-5] there could be a replay attack, where the attacker calls selectWinner when the statement
+    //   `uint256(keccak256(abi.encodePacked(msg.sender, block.timestamp, block.difficulty))) % players.length` results in his playerIndex, so he is the chosen winner
+    // v: [H-5] the same could be done with the statement `uint256(keccak256(abi.encodePacked(msg.sender, block.difficulty))) % 100` to get a legendary rarity
+    // q: not entirely sure if this is an issue, but in the protocol description it does only say that the winner will get a puppy NFT. But nothing regarding a price pool could be found.
     function selectWinner() external {
         require(block.timestamp >= raffleStartTime + raffleDuration, "PuppyRaffle: Raffle not over");
         require(players.length >= 4, "PuppyRaffle: Need at least 4 players");
         uint256 winnerIndex =
             uint256(keccak256(abi.encodePacked(msg.sender, block.timestamp, block.difficulty))) % players.length;
         address winner = players[winnerIndex];
+        // v: [H-2] This is bad practice. When players refund their entranceFee, they are getting it back but their entry in the `players` array is set to the zero address.
+        //    [H-2] So the players.length does not change and you are sending more money than you have collected.
         uint256 totalAmountCollected = players.length * entranceFee;
         uint256 prizePool = (totalAmountCollected * 80) / 100;
         uint256 fee = (totalAmountCollected * 20) / 100;
@@ -161,7 +171,7 @@ contract PuppyRaffle is ERC721, Ownable {
         uint256 tokenId = totalSupply();
 
         // We use a different RNG calculate from the winnerIndex to determine rarity
-        // i: this could also be exploited when calling the statement when its resulting in a legendary rarity
+        // v: [H-5] this could also be exploited when calling the statement when its resulting in a legendary rarity
         uint256 rarity = uint256(keccak256(abi.encodePacked(msg.sender, block.difficulty))) % 100;
         if (rarity <= COMMON_RARITY) {
             tokenIdToRarity[tokenId] = COMMON_RARITY;
@@ -180,9 +190,9 @@ contract PuppyRaffle is ERC721, Ownable {
     }
 
     /// @notice this function will withdraw the fees to the feeAddress
-    // audit: using this.balance is not advised, as you can force ETH into the contract by selfdestructing it, resulting that the owner can never call this function again
-    // v: everybody can call this function, which could lead to unexpected behavior - try to add the onlyOwner modifier
-    // q: When the attacker calls this function and a lot of fees have acquired, may there be any significant impact to the protocol?
+    // v: [H-4] using this.balance is not advised, as you can force ETH into the contract by selfdestructing it, resulting that the owner can never call this function again
+    // v: [L-1] everybody can call this function, which could lead to unexpected behavior - try to add the onlyOwner modifier
+    // v: [L-1] When the attacker calls this function and a lot of fees have acquired, may there be any significant impact to the protocol?
     function withdrawFees() external {
         require(address(this).balance == uint256(totalFees), "PuppyRaffle: There are currently players active!");
         uint256 feesToWithdraw = totalFees;
